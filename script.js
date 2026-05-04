55a8b685854f34cc3bee2faa40f8a74bb529e3b2
hajarzayani79-wq <hajarzayani79@gmail.com>
2026-05-04 22:42:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-function displayTasks() {
-  const list = document.getElementById("taskList");
-  list.innerHTML = "";
-
-  tasks.forEach((task, index) => {
-    let li = document.createElement("li");
-    li.textContent = task.text;
-
-    if (task.done) li.classList.add("done");
-
-    li.onclick = () => toggleTask(index);
-
-    let btn = document.createElement("button");
-    btn.textContent = "X";
-    btn.onclick = (e) => {
-      e.stopPropagation();
-      deleteTask(index);
-    };
-
-    li.appendChild(btn);
-    list.appendChild(li);
-  });
-
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
 function addTask() {
   let input = document.getElementById("taskInput");
 
@@ -35,16 +7,3 @@ function addTask() {
     displayTasks();
   }
 }
-
-function toggleTask(index) {
-  tasks[index].done = !tasks[index].done;
-  displayTasks();
-}
-
-function deleteTask(index) {
-  tasks.splice(index, 1);
-  displayTasks();
-}
-
-// تشغيل عند البداية
-displayTasks();
